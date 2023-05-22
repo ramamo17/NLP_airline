67aba56b5bf537f43a93ce942180deb2dbd9861c
ramamo17 <ramadani.morgan@free.fr>
2023-05-22 15:29:02 +0200

    beautiful soup exemple de towarddatascience

--- scraping.py
+++ scraping.py
@@ -3,29 +3,55 @@ import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+# %%
+session = requests.Session()
 
-def scrape_avis_google(url):
-    # Envoie une requête GET à l'URL donnée
-    response = requests.get(url)
-    
-    # Vérifie si la requête a réussi
-    if response.status_code == 200:
-        # Utilise BeautifulSoup pour analyser le contenu HTML de la page
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Trouve tous les éléments d'avis sur la page
-        avis = soup.find_all('div', class_='review')
-        
-        # Parcours chaque avis et extrait les informations souhaitées
-        for a in avis:
-            note = a.find('span', class_='rating').text
-            commentaire = a.find('span', class_='review-text').text
-            print(f"Note : {note}")
-            print(f"Commentaire : {commentaire}")
-            print('---')
-    else:
-        print("Échec de la requête.")
-
-# Appel de la fonction avec l'URL de la page des avis Google
-scrape_avis_google('https://www.google.com/maps/place/Restaurant+Example/reviews')
-
+# %%
+my_headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS          X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko)          Chrome/71.0.3578.98 Safari/537.36",           "Accept":"text/html,application/xhtml+xml,application/xml;          q=0.9,image/webp,image/apng,*/*;q=0.8"}
+# %%
+url = 'https://communityfoundations.ca/find-a-community-foundation/'
+response = session.get(url, headers=my_headers)
+# %%
+html_soup = BeautifulSoup(response.text, 'html.parser')
+# %%
+container = html_soup.find_all(["h2", "h3"], 
+                               class_=lambda x: x != 'hidden')
+# %%
+url = 'https://www.tripadvisor.fr/Airline_Review-d8728997-Reviews-Air-Botswana'
+response_ta = session.get(url, headers=my_headers)
+soup = BeautifulSoup()
+reviews = soup.find_all('span')
+# %%
+for lines in reviews:
+    if lines.name == 'h2': 
+        province = lines.text
+        print('In', province, "\n")
+    if lines.name == 'h3':
+        foundation = lines.text
+        print('Foundation name:', foundation)   
+        print('Foundation url:', lines.find_all("a", 
+            href=re.compile("cfc_locations"))[0].get('href'), "\n")
+# %%
+subresponse = []
+for lines in container:
+    if lines.name == 'h3': 
+        url_fou = lines.find_all("a", href=re.compile("cfc_locations"))[0].get('href')
+        subresponse.append(session.get(url_fou, 
+                                       headers=my_headers))
+        time.sleep(2)
+# %%
+df = pd.DataFrame({'Organization': organization,
+                   'Title': gender_title,
+                   'Addressee': person,
+                   'Addressee Job Title': person_title,
+                   'Civic Address 1 (Street Address)': street,
+                   'Civic Address 2 (PO Box)': pobox,
+                   'Municipality': municipality,
+                   'Province or Territory': provinces, 
+                   'Postal Code': postalCode,
+                   'Phone': phone,
+                   'Website': org_url
+                   })
+cols = ['Organization', 'Title', {add in here the others}]
+df.to_csv('data/cfcMailingAddresses.csv', encoding='utf-8',
+          index=False, columns=cols)
