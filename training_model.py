9f2631693f9b8e2b4a72bbf59a46c81aa048d360
ramamo17 <ramadani.morgan@free.fr>
2023-05-29 21:39:41 +0200

    rename

--- training_model.py
+++ training_model.py
@@ -2,11 +2,8 @@
 import tensorflow as tf
 import pandas as pd 
 import numpy as np
-import sklearn
-import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import MultinomialNB
